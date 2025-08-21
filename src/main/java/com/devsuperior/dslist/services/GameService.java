50c29e658fb7109d88085ca62464dab383d41e8c
Lazaro Nogueira <lzjunior.007@gmail.com>
2025-08-21 00:11:04 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
+
 
 //Componente do sistema, sendo intermediário entre a classe de acesso ao banco e o controlador REST, devolve dto
 @Component // ou @Service
@@ -17,7 +21,16 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	// busca por id
+	// Obedecer o ACIDI
+	@Transactional(readOnly = true) // Asseguro que nao vou fazer nenhuma operacao de escrita
+	public GameDTO findbyId(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
 	//busca todos os games do banco	
+	@Transactional(readOnly = true) // Asseguro que nao vou fazer nenhuma operacao de escrita
 	public List<GameMinDTO> findall(){ 
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList(); // sequência de dados, transforma um objeto de um tipo pra outro, pra voltar para uma lista normal usa tolist
